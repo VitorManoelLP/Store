530a8ba3c247f4f8bd550c284a3719ab097e9527
= <vitormanoellimapalma@gmail.com>
2022-06-26 09:49:19 -0300

    fix/merge conflicts

--- server/store/src/main/java/com/project/store/resources/UsuarioResource.java
+++ server/store/src/main/java/com/project/store/resources/UsuarioResource.java
@@ -1,9 +1,6 @@
 package com.project.store.resources;
 
 import com.project.store.domain.Usuario;
-import com.project.store.repository.UsuarioRepository;
-import com.project.store.domain.Produto;
-import com.project.store.repository.ProdutoRepository;
 import com.project.store.resources.abstracts.AbstractResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- server/store/src/main/java/com/project/store/resources/abstracts/AbstractResource.java
+++ server/store/src/main/java/com/project/store/resources/abstracts/AbstractResource.java
@@ -3,33 +3,16 @@ package com.project.store.resources.abstracts;
 import com.project.store.domain.interfaces.DomainImp;
 import com.project.store.service.interfaces.ServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
->>>>>>> 22f8cdf31a543df57e12b6b192354ac545d6ba87
-import org.springframework.data.jpa.repository.JpaRepository;
-=======
->>>>>>> Stashed changes
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-@CacheConfig(cacheNames = "cacheResource")
->>>>>>> 22f8cdf31a543df57e12b6b192354ac545d6ba87
-public abstract class AbstractResource<E extends DomainImp<Long>, R extends JpaRepository<E, Long>> {
-=======
 public abstract class AbstractResource<E extends DomainImp<Long>> {
->>>>>>> Stashed changes
-
     @Autowired
     private ServiceImp<E> service;
 
